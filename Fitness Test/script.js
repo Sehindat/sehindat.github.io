6bc5150f813f901103be3e1d859987c04a2b9a95
Sehindat <101187811+Sehindat@users.noreply.github.com>
2022-03-10 15:02:57 +0100

    Add files via upload
    initial commit.

---
+++ Fitness Test/script.js
@@ -0,0 +1,48 @@
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "red"
+    event.target.style.color = "white"
+});
+
+const change2 = document.getElementById("image2");
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "purple";
+    event.target.style.color = "red";
+});
+
+const change3 = document.getElementById("image3");
+change3.addEventListener("click",(event) => {
+    event.target.style.background = "gold";
+    event.target.style.color = "black";
+});
+
+const change4 = document.getElementById("image4");
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "blue";
+    event.target.style.color = "black";
+});
+
+const change5 = document.getElementById("image5");
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("image6");
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "white";
+});
+
+const change7 = document.getElementById("image7");
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "blue";
+});
+
+const change8 = document.getElementById("image8");
+change8.addEventListener("click", (event) => {
+    event.target.style.background = "yellow";
+    event.target.style.color = "white";
+});
